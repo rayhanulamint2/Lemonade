9d47d6de2a05fc1462ff25a8085fa74fda4a3359
2020331002_Tanvir <95771942+rayhanulamint2@users.noreply.github.com>
2024-01-27 23:33:14 +0600

    adding shadow in images

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
@@ -28,6 +30,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -109,18 +112,29 @@ fun Lemonade(modifier: Modifier = Modifier){
                     horizontalAlignment = Alignment.CenterHorizontally,
                     modifier = Modifier.padding(16.dp)
                 ) {
-                    Button(onClick = {
-                        result = when (result) {
-                            1 -> 2
-                            2 -> 3
-                            3 -> 4
-                            else -> 1
-                        }
-                    },
-                        shape = RoundedCornerShape(40.dp),
-                        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
+                    Surface(shadowElevation = 8.dp,
+                        shape = RoundedCornerShape(40.dp)
                     ) {
-                        Image(painter = image, contentDescription = "1")
+                        Button(onClick = {
+                            result = when (result) {
+                                1 -> 2
+                                2 -> 3
+                                3 -> 4
+                                else -> 1
+                            }
+                        },
+                            shape = RoundedCornerShape(40.dp),
+                            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
+                        ) {
+                            Image(
+                                painter = image,
+                                contentDescription = "1",
+                                modifier = Modifier
+                                    .width(128.dp)
+                                    .height(160.dp)
+                                    .padding(24.dp)
+                            )
+                        }
                     }
                     Spacer(modifier = modifier.padding(16.dp))
 
