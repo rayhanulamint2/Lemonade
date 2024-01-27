20f5f5297dd5cec55ad0cf78cd587f61bc882518
2020331002_Tanvir <95771942+rayhanulamint2@users.noreply.github.com>
2024-01-27 22:03:35 +0600

    third commit - topbar setup

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -10,10 +10,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,9 +28,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,6 +54,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Lemonade(modifier: Modifier = Modifier){
     var result by remember {
@@ -69,28 +78,62 @@ fun Lemonade(modifier: Modifier = Modifier){
     )
     val tanvir = 1
 
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.padding(16.dp)
-    ) {
-        Button(onClick = { result = when(result){
-            1 -> 2
-            2 -> 3
-            3 -> 4
-            else ->1
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Lemonade",
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            )
+        },
+        content = {
+            innerpadding ->
+            Surface (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerpadding)
+                    .background(MaterialTheme.colorScheme.tertiaryContainer),
+                color = MaterialTheme.colorScheme.background
+            ){
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Button(onClick = {
+                        result = when (result) {
+                            1 -> 2
+                            2 -> 3
+                            3 -> 4
+                            else -> 1
+                        }
+                    },
+                        shape = RoundedCornerShape(40.dp),
+                        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
+                    ) {
+                        Image(painter = image, contentDescription = "1")
+                    }
+                    Spacer(modifier = modifier.padding(16.dp))
+
+                    Text(
+                        text = text1,
+                        fontSize = 18.sp
+                    )
+
+                }
             }
-        }) {
-            Image(painter = image, contentDescription = "1")
+
         }
-        Spacer(modifier = modifier.padding(16.dp))
-
-        Text(
-            text = text1,
-            fontSize = 18.sp
-        )
-
-    }
+    )
 }
 
 @Preview(showBackground = true,
